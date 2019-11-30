159c0fe7e44cd26cc41174aa9d1a328963787bb5
AndyMcElligott <andymc2315@gmail.com>
2019-11-30 17:06:34 -0600

    delete and put routers created

--- server/routes/todo.router.js
+++ server/routes/todo.router.js
@@ -26,4 +26,32 @@ router.post('/', (req, res)=>{
     })
 })
 
+//PUT
+router.put('/:id', (req, res) => {
+    const list = req.body;
+    const id = [req.params.id];
+    const queryText = `UPDATE "list" SET "status" = 'true' WHERE "id"= $1`;
+    pool.query(queryText, id)
+        .then(result => {
+            res.sendStatus(201);
+    })
+        .catch(error => {
+            console.log('error in PUT ROUTER', error);
+            res.sendStatus(500);
+    });
+});
+
+//DELETE
+router.delete('/:id', (req, res) =>{
+    console.log('in ROUTER DELETE, deleting id:', id);
+    let id = req.params.id;
+    let queryString = 'DELETE FROM "tasks" WHERE "id"=$1;';
+    pool.query(queryString, [id]).then(result =>{
+        res.sendStatus(200)
+    }).catch(error =>{
+        console.log('ERROR DELETING TASK ------------>', error);
+        res.sendStatus(500)
+    })
+})
+
 module.exports = router;
