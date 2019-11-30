4d886889935b5f8101fc4b730e04b0d493d781db
AndyMcElligott <andymc2315@gmail.com>
2019-11-30 16:13:11 -0600

    ROUTER POST created

--- server/routes/todo.router.js
+++ server/routes/todo.router.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const pool = require('../modules/pool');
 
+// GET
 router.get('/', (req, res)=>{
     console.log('in ROUTER GET');
     let queryString = `SELECT * FROM list;`;
@@ -9,6 +10,19 @@ router.get('/', (req, res)=>{
         res.send(result.rows);
     }).catch(error =>{
         console.log('ERROR GETTING TASKS ON SERVER ------------>', error);
+    })
+})
+
+//POST
+router.post('/', (req, res)=>{
+    let thingOne = req.body;
+    console.log('Adding new task to list', thingOne);
+    //insert into list DB by id
+    let queryString = `INSERT INTO "list" ("task") VALUES ($1, 'false');`;
+    pool.query(queryString, [thingOne.task]).then(result =>{
+        res.sendStatus(201);
+    }).catch(error => {
+        res.sendStatus(500);
     })
 })
 
